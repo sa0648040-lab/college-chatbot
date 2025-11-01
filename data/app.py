6cb355cd3450799d763df7729eeaa68fefb2bb44
sa0648040-lab <sa0648040@gmail.com>
2025-11-02 02:40:34 +0530

    Create app.py

---
+++ data/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("College Chatbot - TDB College")
+
+queries = {
+    "exam": "Exams start on Dec 10, 2025.",
+    "fees": "Hostel fees: ₹50,000/year.",
+    "admission": "Apply at www.tdbcollege.ac.in/admissions",
+    "library": "Open 9 AM - 5 PM",
+    "hostel": "Apply in admin office",
+    "timetable": "Monday: Maths 9AM, Physics 10AM"
+}
+
+user = st.text_input("Ask anything:").lower()
+
+if user:
+    if "exam" in user:
+        st.write("Bot:", queries["exam"])
+    elif "fee" in user:
+        st.write("Bot:", queries["fees"])
+    elif "admission" in user:
+        st.write("Bot:", queries["admission"])
+    elif "library" in user:
+        st.write("Bot:", queries["library"])
+    elif "hostel" in user:
+        st.write("Bot:", queries["hostel"])
+    elif "timetable" in user:
+        st.write("Bot:", queries["timetable"])
+    else:
+        st.write("Bot: Sorry, try asking about exams, fees, or admission.")
