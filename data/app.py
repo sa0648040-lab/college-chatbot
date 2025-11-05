44e152957453a554cc5c6f7c504c8d4606efb780
sa0648040-lab <sa0648040@gmail.com>
2025-11-05 18:16:53 +0530

    Update app.py

--- data/app.py
+++ data/app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.title("College Chatbot - TDB College")
+st.title("College Chatbot - Bits Pilani")
 
 queries = {
     "exam": "Exams start on Dec 10, 2025.",
     "fees": "Hostel fees: ₹50,000/year.",
-    "admission": "Apply at www.tdbcollege.ac.in/admissions",
+    "admission": "Apply at bits-pilani.ac.in/admissions",
     "library": "Open 9 AM - 5 PM",
     "hostel": "Apply in admin office",
     "timetable": "Monday: Maths 9AM, Physics 10AM"
